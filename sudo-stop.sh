5fe91c7f93ab9691ebd6a7cbec6151a82b9d3ee0
kyle-thdi <123032727+kyle-thdi@users.noreply.github.com>
2023-01-20 13:58:32 -0400

    Create sudo-stop.sh

---
+++ sudo-stop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+container_name = nginx-proxy-manager
+
+# ensure docker package intalled
+if ! command -v docker  &> /dev/null
+then
+    echo "The \"docker\" command could not be found - install docker and run this script again"
+    exit
+fi
+
+# ensure docker-compose installed
+if ! command -v docker-compose &> /dev/null; then
+    echo "The \"docker-compose\" could not be found - install docker-compose and run this script again"
+    exit
+fi
+
+# ensure container is actually running
+if [ "$( docker container inspect -f '{{.State.Status}}' $container_name )" == "running" ]; then 
+   echo "Shutting down container $conatainer_name now"
+   cd docker
+   sudo docker-compose down
+fi
