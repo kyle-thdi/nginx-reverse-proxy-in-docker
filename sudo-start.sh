1fd4baa481f9f1b45d667f9b788779c4f7ac29ea
Kyle Thompson <123032727+kyle-thdi@users.noreply.github.com>
2023-01-20 23:34:19 -0400

    Update sudo-start.sh

--- sudo-start.sh
+++ sudo-start.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
+# set up variables
+DIRECTORY=docker
+FILE=docker-compose.yml
+CLONE_MSG="Use \"git clone https://github.com/kyle-thdi/nginx-reverse-proxy-in-docker.git\" again to ensure you have all required files"
+
 # ensure required directories exist, if not, create them
 mkdir -p data
 mkdir -p letsencrypt
 
 # ensure docker package intalled
 if ! command -v docker  &> /dev/null; then
-    echo "The \"docker\" command could not be found - install docker and run this script again"
+    echo "The \"docker\" command could not be found. $CLONE_MSG."
     exit
 fi
 
 # ensure docker-compose installed
 if ! command -v docker-compose &> /dev/null; then
-    echo "The \"docker-compose\" could not be found - install docker-compose and run this script again"
+    echo "The \"docker-compose\" could not be found. $CLONE_MSG."
     exit
 fi
 
-# Bring up docker-compose to run in background (daemon)
+# ensure docker directory exists
+if [ ! -d "$DIRECTORY" ]; then
+  echo "The $DIRECTORY directory does not exist and is required. $CLONE_MSG."
+  exit
+fi
 
-FILE=docker-compose.yml
+# change to docker directory and ensure that docker-install.yml exits
+
+cd $DIRECTORY
 
 if [ -f "$FILE" ]; then
     # docker-compose.yml found, run docker-compose up -d
-    cd docker
     sudo docker-compose up -d
 else 
-    echo "$FILE does not exist and is required. Exiting."
+    echo "$FILE does not exist and is required. $CLONE_MSG."
     exit
 fi
